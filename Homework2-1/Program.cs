d599993d39e1cdcfc85ca9ebf02e8aaed1a62783
Marat Islamov <Razr1629@gmail.com>
2022-06-30 20:49:05 +0500

    homework

---
+++ Homework2-1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    Console.WriteLine("Введите число");
+    string number = Console.ReadLine();
+    homework2(number);
+
+    void homework2(string number)
+    {
+    if (number.Length < 3)
+    Console.WriteLine("нет третьей цифры");
+    else
+    Console.WriteLine($"{number[2]} третья цифра числа {number}");
+   
+    }
+            
+        
+    }
+}
---
+++ Homework2-2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Напишите программу, которая принимает на
+//вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ведите число дня недели для проверки выходнной это или нет");
+        int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+        if (DayOfWeek == 1) Console.WriteLine("нет");
+        else if (DayOfWeek == 2) Console.WriteLine("нет");
+        else if (DayOfWeek == 3) Console.WriteLine("нет");
+        else if (DayOfWeek == 4) Console.WriteLine("нет");
+        else if (DayOfWeek == 5) Console.WriteLine("нет");
+        else if (DayOfWeek == 6) Console.WriteLine("да");
+        else if (DayOfWeek == 7) Console.WriteLine("да");
+        else Console.WriteLine("нет такого дня недели");        
+    }
+}
